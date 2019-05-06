a561447cb73e73e1bf0ef23a19cf6e646ed27408
andband <finnhoulihan@email.com>
2019-05-06 03:03:03 +0100

    security config

--- src/main/java/com/andband/accounts/config/security/ResourceServerConfig.java
+++ src/main/java/com/andband/accounts/config/security/ResourceServerConfig.java
@@ -1,7 +1,6 @@
-package com.andband.accounts.security;
+package com.andband.accounts.config.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -16,7 +15,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().hasAuthority("ROLE_INTERNAL_API");
+                .anyRequest().hasAuthority("ROLE_INTERNAL_API")
+                .and()
+                .csrf().disable();
     }
 
 }
